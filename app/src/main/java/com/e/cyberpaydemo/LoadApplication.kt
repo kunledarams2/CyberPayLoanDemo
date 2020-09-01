f66d51ca039d00fd15f703e96a0169016dfffa26
kunledarams2 <Adekunle@1>
2020-09-01 14:18:03 +0100

    first push

---
+++ app/src/main/java/com/e/cyberpaydemo/LoadApplication.kt
@@ -0,0 +1,38 @@
+package com.e.cyberpaydemo
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class LoadApplication : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_load_application, container, false)
+    }
+
+    companion object {
+
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            LoadApplication().apply {
+                arguments = Bundle().apply {
+
+                }
+            }
+    }
+}
